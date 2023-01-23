d83dfe21a645f358b0178cf7aa9ae5874cb13993
Nikolay Lapshin <nlapshin1989@gmail.com>
2023-01-23 21:11:33 +0300

    Added upload reports

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ export default {
   testMatch: ['**/src/index.spec.js'],
   "transform": {
     "^.+\\.[t|j]sx?$": "babel-jest"
-  }
+  },
+  reporters: [
+    "default", 
+    [
+      "jest-html-reporters", {
+        "publicPath": "./jest-html-report",
+        "filename": "report.html"
+      }
+    ]
+  ]
 }
