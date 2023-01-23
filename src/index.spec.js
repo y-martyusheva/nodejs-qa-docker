0cd31734e51022bf09fb6813caa276bd5ebc3052
Nikolay Lapshin <39495311+nlapshin@users.noreply.github.com>
2023-01-23 20:56:35 +0300

    Broke test

--- src/index.spec.js
+++ src/index.spec.js
@@ -22,7 +22,7 @@ describe('GET /hello/:name', () => {
 
   test('Should return welcome string 2', async () => {
     const res = await supertest(config.url)
-      .get('/hello/anon')
+      .get('/hello/bad-user')
       .set('Accept', 'application/json');
 
     expect(res.body).toEqual({ msg: 'Hello anon from express' })
